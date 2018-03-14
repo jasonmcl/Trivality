6de93f2799af1e37e5cb27ebc367019b9bdd6082
Jason McLaughlin <jasmclaughlin13@gmail.com>
2018-03-14 10:17:17 -0700

    Created api controller and routes for create, read, update, delete

---
+++ Trivality.Web/Controllers/AccountsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Trivality.Web.Controllers
+{
+    [RoutePrefix("api/accounts")]
+    public class AccountsController : ApiController
+    {
+        [Route, HttpGet]
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Data");
+        }
+
+        [Route("{id:int}"), HttpGet]
+        public HttpResponseMessage Get(int id)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Heres your id: " + id);
+        }
+
+        [Route, HttpPost]
+        public HttpResponseMessage Post([FromBody]string value)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Posted");
+        }
+
+        [Route("{id:int}"), HttpPut]
+        public HttpResponseMessage Put(int id, [FromBody]string value)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Updated");
+        }
+
+        [Route("{id:int}"), HttpDelete]
+        public HttpResponseMessage Delete(int id)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Deleted");
+        }
+    }
+}
