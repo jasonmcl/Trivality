2f812f5ef92011ad2787f61da97fff683fc0d103
Jason McLaughlin <jasmclaughlin13@gmail.com>
2018-03-14 08:20:33 -0700

    Created unit test for the insert method

---
+++ Trivality.UnitTests/AccountsTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trivality.Models.Requests;
+using Trivality.Services;
+
+namespace Trivality.UnitTests
+{
+    [TestClass]
+    public class AccountsTest
+    {
+        [TestMethod]
+        public void InsertTest()
+        {
+            AccountAddRequest model = new AccountAddRequest()
+            {
+                Username = "username",
+                Email = "email@email.com",
+                ModifiedBy = "Unit Test"
+            };
+            AccountService svc = new AccountService();
+            int result = svc.Insert(model);
+            Assert.IsTrue(result > 0);
+        }
+    }
+}
