a117832878f906e11882d89dc7eab3c188312ea2
Jason McLaughlin <jasmclaughlin13@gmail.com>
2018-03-14 07:40:44 -0700

    Created the insert method, to insert accounts into the database

---
+++ Trivality.Models/Requests/AccountAddRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivality.Models.Requests
+{
+    public class AccountAddRequest
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
---
+++ Trivality.Services/AccountService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trivality.Models.Requests;
+
+namespace Trivality.Services
+{
+    public class AccountService
+    {
+        private string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        
+        public int Insert(AccountAddRequest model)
+        {
+            int id = 0;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string cmdText = "accounts_insert";
+                using (SqlCommand cmd = new SqlCommand(cmdText, conn))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    SqlParameter param = new SqlParameter();
+                    param.ParameterName = "@Id";
+                    param.SqlDbType = System.Data.SqlDbType.Int;
+                    param.Direction = System.Data.ParameterDirection.Output;
+
+                    cmd.Parameters.Add(param);
+                    cmd.Parameters.AddWithValue("@Username", model.Username);
+                    cmd.Parameters.AddWithValue("@Email", model.Email);
+                    cmd.Parameters.AddWithValue("@ModifiedBy", model.ModifiedBy);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    id = (int)cmd.Parameters["@Id"].Value;
+                    conn.Close();
+                }
+            }
+            return id;
+        }
+    }
+}
